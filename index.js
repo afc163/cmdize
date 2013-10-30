038b611b81c30014c66dd3ed58fb71654387ce0c
afc163 <afc163@gmail.com>
2013-10-30 15:39:39 +0800

    fix #2

--- index.js
+++ index.js
@@ -1,54 +1,25 @@
 var fs = require('fs');
 var path = require('path');
-var exists = fs.existsSync || path.existsSync;
-var spawn = require('win-spawn');
 var wrapper = require('./lib/cmd-wrapper');
+var checkType = require('./lib/check-type');
+var detectGlobal = require('./lib/detect-global');
 
 module.exports = function(code, options, callback) {
 
   code = code.toString().trim();
+  var type = checkType(code);
+  console.log(type);
 
-  if (supportCommonJS(code)) {
-    callback && callback(wrapper(code));
-    return;
+  if (type === 'jQuery' || type === 'CommonJS' || type === 'jQueryPlugin') {
+    callback && callback(wrapper(code, {
+      type: type
+    }));
+  } else {
+    detectGlobal(code, function(variables) {
+      var result = wrapper(code, options, variables);
+      callback && callback(result);
+    });
   }
-
-  fs.writeFileSync('/tmp/cmdize-tmp.html', '<script>' + code + '</script>');
-
-  var phantomjs;
-  for (var i=0; i < module.paths.length; i++) {
-    var bin = path.join(module.paths[i], '.bin/phantomjs');
-    if (process.platform === 'win32') {
-      bin += '.cmd';
-    }
-    if (exists(bin)) {
-      phantomjs = bin;
-      break;
-    }
-  }
-  if (phantomjs === undefined) {
-    phantomjs = 'phantomjs';
-  }
-
-  var subprocess = spawn(phantomjs, [__dirname + '/lib/phantomjs-script.js']);
-
-  subprocess.stdout.on('data', function(data) {
-    var variables = data.toString().replace(/\n$/, '');
-    if (variables !== '') {
-      console.log('Found global variables need to export: '.gray + variables.magenta);
-      variables = variables.split(',');
-    } else {
-      console.log('No found any global variables.'.gray);
-      variables = [];
-    }
-    var result = wrapper(code, variables, options);
-    callback && callback(result);
-  });
 
 };
 
-function supportCommonJS(code) {
-  if (code.indexOf('module.exports') > 0) {
-    return true;
-  }
-}
